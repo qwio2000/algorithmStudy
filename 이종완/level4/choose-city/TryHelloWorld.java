c5dd1e55df89d8b2915b755449856202bbb3f97b
Jongwan Lee <jjmean2@gmail.com>
2017-11-17 23:09:57 +0900

    Level4 공항건설하기 문제 풀이 추가

--- 이종완/level4/choose-city/TryHelloWorld.java
+++ 이종완/level4/choose-city/TryHelloWorld.java
@@ -4,10 +4,96 @@ import java.util.stream.Collectors;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.function.BiFunction;
+import java.util.Map;
+import java.util.HashMap;
+
 
 class TryHelloWorld
 {
-    public int chooseCity(int n, int [][]city) {
+    public Map<Integer, BiFunction<Integer, int[][], Long>> algorithms = new HashMap<>();
+
+    public TryHelloWorld() {
+        this.algorithms.put(0, this::chooseCity);
+        this.algorithms.put(1, this::chooseCity1);
+        this.algorithms.put(2, this::chooseCity2);
+        this.algorithms.put(3, this::chooseCity3);
+    }
+
+    public long chooseCity3(int n, int[][] city)
+    {
+        int answer = 0;
+        int[] cost = new int[n];
+
+        for(int i = 1; i < n; i++) {
+          for(int j = 0; j < i; j++) {
+            long distance = Math.abs(city[i][0] - city[j][0]);
+            cost[i] += city[j][1] * distance;
+            cost[j] += city[i][1] * distance;
+          }
+        }
+
+        int minCost = Integer.MAX_VALUE;
+        int minIdx = 0;
+        for(int i = 0; i < n; i++) {
+          if(minCost > cost[i]) {
+            minCost = cost[i];
+            minIdx = i;
+          }
+        }
+
+
+        return city[minIdx][0];
+}
+
+    // 다른 사람 풀이
+    public long chooseCity2(int n , int[][] city) {
+        int tmp = 0;
+        int answer = 0;
+        for(int i = 0; i<city.length; i++ ) {
+            if(tmp <city[i][1]) {
+                tmp = city[i][1];
+                answer = i;
+            }
+        }
+        return city[answer][0];
+    }
+
+    public long chooseCity1(int n, int [][]city) {
+        city = Arrays.copyOf(city, city.length);
+        Arrays.sort(city, (before, after) -> Integer.compare(before[0], after[0]));
+
+        int selectedIndex = city.length-1;
+        int increment = 0; 
+        for (int i = 0; i < city.length; i++) {
+            increment -= city[i][1];
+        }
+
+        for (int i = 0; i < city.length; i++) {
+            increment += city[i][1] * 2;
+            if (increment >= 0) {
+                selectedIndex = i;
+                break;
+            }
+        }
+
+        return city[selectedIndex][0];
+    }
+
+    int getIncrement(int index, int[][] city) {
+        int increment = 0;
+        for (int i = 0; i < city.length; i++) {
+            if (i <= index) {
+                increment += city[i][1];
+            } else {
+                increment += -city[i][1];
+            }
+        }
+        return increment;
+    }
+
+    public long chooseCity(int n, int [][]city) {
+        System.out.println(toArrayString(city));
         int length = city.length;
 
         int selectedIndex = -1;
@@ -20,6 +106,9 @@ class TryHelloWorld
                 int d = city[i][0] - city[j][0];
                 d = d < 0 ? -d : d;
                 distance += d * city[j][1];
+                //if (i > 0 && distance > minDistance) {
+                    //break;
+                //}
             }
             if (i == 0) {
                 selectedIndex = i;
@@ -49,6 +138,16 @@ class TryHelloWorld
         return cityInfo;
     }
 
+    public static String toArrayString(int[][] array) {
+        String result = "[";
+        result += Stream.of(array)
+            .map(s -> Arrays.toString(s))
+            .collect(Collectors.joining(", "));
+        result += "]";
+
+        return result;
+    }
+
     public static void main(String[] args) {
         List<int[]> cityList = new ArrayList<>();
         Scanner scan = new Scanner(System.in);
@@ -56,7 +155,7 @@ class TryHelloWorld
         int count = 0;
         while(true) {
             count++;
-            System.out.printf("도시 %d: ", count);
+            //System.out.printf("도시 %d: ", count);
             input = scan.nextLine();
             if (input.equals("q")) {
                 break;
@@ -64,6 +163,25 @@ class TryHelloWorld
             cityList.add(getIntArrayFromInput(input));
         }
         System.out.println("\n");
+
+        int numberOfAlgorithm = 0;
+        BiFunction<Integer, int[][], Long> algorithm = null;
+
+        if (args.length >= 1) {
+            try {
+                numberOfAlgorithm = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+
+            }
+        }
+
+        TryHelloWorld test = new TryHelloWorld();
+
+        algorithm = test.algorithms.get(numberOfAlgorithm);
+        if (algorithm == null) {
+            numberOfAlgorithm = 0;
+            algorithm = test.algorithms.get(numberOfAlgorithm);
+        }
 
         int tn = 3;
         int [][] tcity = {{1, 5}, {2, 2}, {3, 3}};
@@ -73,15 +191,14 @@ class TryHelloWorld
             tn = cityList.size();
         }
 
-        TryHelloWorld test = new TryHelloWorld();
         long started = System.currentTimeMillis();
-        int result = test.chooseCity(tn, tcity);
+        long result = algorithm.apply(tn, tcity);
         long elapsed = System.currentTimeMillis() - started;
 
-        System.out.printf("문제: %d개의 도시\n[%s]\n", tn,
-                Stream.of(tcity)
-                    .map(info -> Arrays.toString(info))
-                    .collect(Collectors.joining(", "))); 
+        System.out.println("=========================================");
+        System.out.printf("문제: %d 개의 도시\n%s\n\n", tn, toArrayString(tcity)); 
+        System.out.println("-----------------------------------------");
+        System.out.println("방식: " + numberOfAlgorithm);
         System.out.printf("답안: %d\n", result);
         System.out.printf("elapsed time: %d ms\n\n", elapsed);
     }
