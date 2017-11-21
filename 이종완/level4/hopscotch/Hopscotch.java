074cf52d47510e1c3aba34d89de01595e6bba67b
Jongwan Lee <jjmean2@gmail.com>
2017-11-21 12:18:05 +0900

    backup temp

--- 이종완/level4/hopscotch/Hopscotch.java
+++ 이종완/level4/hopscotch/Hopscotch.java
@@ -1,69 +1,51 @@
 class Hopscotch {
 
     int hopscotch(int[][] board, int size) {
-        int length = board.length;
-        int maxScore = findMaxScore(board, size, 0);
-        for (int i = 1; i < length; i++) {
-            int score = findMaxScore(board, size, i);
-            if (score > maxScore) {
-                maxScore = score;
-            }
-        }
-        return maxScore;
+        int[][] selection = new int[board.length][board[0].length];
+
+
+        return 0;
     }
 
-    int findMaxScore(int[][] board, int size, int start) {
-        System.out.printf("%d 부터 시작\n", start);
-        int length = board.length;
-        int previousIndex = -1;
-        int nextIndex = -1;
-        int firstMaxIndex = -1;
-        int sum = 0;
-        int i = start;
-        do {
-            if (i == ((start + length) - 1) % length) {
-                nextIndex = firstMaxIndex;
-            } 
-
-            System.out.printf("%d줄: ", i);
-            int[] row = board[i];
-            boolean started = false;
-            int max = -1;
-            int maxIndex = -1;
-            for (int j = 0; j < row.length; j++) {
-                if (j != previousIndex && j != nextIndex) {
-                    if (started == false) {
-                        started = true;
-                        max = row[j];
-                        maxIndex = j;
-                    } else  {
-                        if (row[j] > max) {
-                            max = row[j];
-                            maxIndex = j;
-                        }
-                    }
+    int getMaxIndex(int[] row, int[] excludeIndexes) {
+        int max = -1;
+        int maxIndex = -1;
+        boolean isBeginning = true;
+        for (int i = 0; i < row.length; i++) {
+            boolean excluded = false;
+            for (int j = 0; j < excludeIndexes.length; j++) {
+                if (i == excludeIndexes[j]) {
+                    excluded = true;
+                    break;
                 }
             }
-            System.out.printf("index: %d, score: %d\n", maxIndex, max);
-            sum += max;
-
-            if (i == start) {
-                firstMaxIndex = maxIndex;
+            if (excluded) {
+                break;
             }
-            if (i != length - 1) {
-                previousIndex = maxIndex;
-            } else {
-                previousIndex = -1;
-            }
-            System.out.printf("firstMax: %d, previous: %d, next: %d\n", firstMaxIndex, previousIndex, nextIndex);
 
-            i = (i + 1) % length;
-        } while (i != start);
-        System.out.printf("\n sum=%d\n\n", sum);
-        return sum;
+            if (isBeginning) {
+                max = row[i];
+                maxIndex = i;
+                isBeginning = false;
+            } else if (row[i] > max) {
+                max = row[i];
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
     }
 
+    void makeSelection(int[] row, int index) {
+        int value = row[index];
+        for (int i = 0; i < row.length; i++) {
+            row[i] -= value;
+        }
+    }
 
+    int getCase(int[][] selection, int index, int score) {
+
+
+    } 
 
     public static void main(String[] args) {
         Hopscotch c = new Hopscotch();
