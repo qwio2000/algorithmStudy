106ba13a2e113369a999ad832221d5c2c82dd628
JSY <qwio2000>
2017-11-24 14:43:21 +0900

    Level4, Level5

--- 전승엽/20171117/ChooseCity.java
+++ 전승엽/20171117/ChooseCity.java
@@ -4,28 +4,16 @@ class ChooseCity
 {
     public long chooseCity(int n, int[][] city) {
       long answer = 0;
-      long[][] distanceCalAry = new long[n][2];
-
-      for (int i = 0; i < city.length; i++) {
-        long distance = 0;
-        for (int j = 0; j < city.length; j++) {
-          if (i == j) continue;
-          distance += Math.abs(city[i][0] - city[j][0]) * city[j][1];
+      long totalManCnt = 0;
+      for(int i=0; i<city.length; i++) totalManCnt = city[i][1];
+      totalManCnt /= 2;
+      for(int i=0; i<city.length; i++) {
+        totalManCnt -= city[i][1];
+        if(totalManCnt <= 0) {
+          answer = city[i][0];
+          break;
         }
-        distanceCalAry[i][0] = city[i][0];
-        distanceCalAry[i][1] = distance;
       }
-
-      ArrayList<Long> answers = new ArrayList<Long>();
-      long minVal = distanceCalAry[0][1];
-      for (int i = 1; i < distanceCalAry.length; i++) {
-        minVal = Math.min(minVal, distanceCalAry[i][1]);
-      }
-      for (int i = 0; i < distanceCalAry.length; i++) {
-        if (distanceCalAry[i][1] == minVal) answers.add(distanceCalAry[i][0]);
-      }
-      Collections.sort(answers);
-      answer = answers.get(0);
       return answer;
     }
 
